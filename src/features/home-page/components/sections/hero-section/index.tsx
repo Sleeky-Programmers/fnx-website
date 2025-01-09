afd3077bffec71bee25b5818378fe0dac3f1626e
Shiloh George <shinohgeorge2019@gmail.com>
2025-01-09 15:11:04 +0100

    made hero section mobile responsive

--- src/features/home-page/components/sections/hero-section/index.tsx
+++ src/features/home-page/components/sections/hero-section/index.tsx
@@ -17,8 +17,10 @@ export function HeroSection() {
 			<div className="z-20 flex flex-col gap-6 items-center justify-center text-white">
 				<p className="font-bold text-center text-[#D5D5D5]">AIFMD and Management Company Services</p>
 				<div className="flex flex-col gap-2 items-center justify-center">
-					<h1 className="font-bold text-5xl text-center">Innovate. Transform. Thrive.</h1>
-					<p className="font-medium text-2xl w-[65%] text-center">Empowering businesses with cutting-edge digital solutions to stay ahead in a fast-changing world</p>
+					<h1 className="font-bold text-3xl md:text-5xl text-center">Innovate. Transform. Thrive.</h1>
+					<p className="font-medium text-lg md:text-2xl w-[65%] text-center">
+						Empowering businesses with cutting-edge digital solutions to stay ahead in a fast-changing world
+					</p>
 				</div>
 				<GetStarted />
 			</div>
