5dc8a452c189d68f6c539b49b09eac104d92e438
Shiloh George <shinohgeorge2019@gmail.com>
2025-01-09 15:20:45 +0100

    Made home page about section mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,18 @@ export default function Home() {
 			<HeroSection />
 
 			<section className="w-full mt-10 flex flex-col items-center">
-				<h2 className="font-bold text-5xl flex flex-col gap-2 items-center justify-center">
+				<h2 className="font-bold text-3xl md:text-5xl flex flex-col gap-1 md:gap-2 items-center justify-center">
 					<span className="text-[#161C2D]">About Us</span>
-					<span className="w-2/4 h-2 rounded-xl mx-auto bg-[#A88873]" />
+					<span className="w-2/4 h-1 md:h-2 rounded-xl mx-auto bg-[#A88873]" />
 				</h2>
 			</section>
 
 			<HomePageCard
 				imageSrc={aboutUsImage}
 				imageTitle="About Us Image">
-				<div className="w-full h-full flex flex-col items-center justify-center gap-10 px-5">
-					<h3 className="font-bold text-3xl">Driving Growth Through Innovation and Expertise</h3>
-					<p className="font-medium text-xl">
+				<div className="w-full h-full flex flex-col items-center justify-center gap-3 md:gap-6 md:px-5">
+					<h3 className="font-bold text-xl md:text-3xl text-center md:text-left">Driving Growth Through Innovation and Expertise</h3>
+					<p className="font-medium text-lg md:text-xl text-justify">
 						Fnx is a global leader in software development, product design, and innovation strategy. Our passionate team delivers tailored solutions that drive business
 						growth and success.
 					</p>
--- src/features/home-page/components/cards/index.tsx
+++ src/features/home-page/components/cards/index.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from 'react';
 export function HomePageCard({ children, reverse, imageSrc, imageTitle }: { children: ReactNode; imageSrc: StaticImageData; imageTitle: string; reverse?: true }) {
 	return (
 		<section className="w-full flex items-center justify-center py-8 px-5 lg:px-11">
-			<section className="w-[95%] h-fit grid lg:grid-cols-10 gap-8 relative">
-				<div className="col-span-6">{children}</div>
-				<div className="col-span-4">
+			<section className="w-[95%] h-fit grid grid-cols-1 md:grid-cols-10 gap-8 relative">
+				<div className="md:col-span-6 row-start-2 md:row-auto">{children}</div>
+				<div className="md:col-span-4 row-start-1 md:row-auto">
 					<Image
 						src={imageSrc}
 						alt={imageTitle}
