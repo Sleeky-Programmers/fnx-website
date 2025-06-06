c0094803302031f5d17a486215528e3c1758950d
Shiloh George <shinohgeorge2019@gmail.com>
2025-06-06 15:48:35 +0100

    Setup Production Data

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
+	// basePath: process.env.BASE_URL || '',
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
